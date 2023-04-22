e0e347de77a2c5188e9189d504a8575e2708da28
mjs-dinesh <dineshmjs22@gmail.com>
2023-04-22 22:10:43 +0530

    initial commit

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Node Js Interview Preparation</span>,
+  project: {
+    link: "https://github.com/shuding/nextra-docs-template",
+  },
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  footer: {
+    text: "Node Js Interview Template",
+  },
+};
+
+export default config;
